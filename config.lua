0aff44a8864a40acfde9146fc6900ee1d25052a6
Charles Capps <charles.capps@jivesoftware.com>
2015-01-31 22:29:25 -0800

    First commit

---
+++ config.lua
@@ -0,0 +1,32 @@
+application =
+{
+
+	content =
+	{
+		width = 750,
+		height = 1334, 
+		scale = "letterBox",
+		fps = 30,
+		
+		--[[
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+		},
+		--]]
+	},
+
+	--[[
+	-- Push notifications
+	notification =
+	{
+		iphone =
+		{
+			types =
+			{
+				"badge", "sound", "alert", "newsstand"
+			}
+		}
+	},
+	--]]    
+}
