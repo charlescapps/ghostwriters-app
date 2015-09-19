e1a1c1e61b9b37e4774a03b0be81acbf4258c59d
Charles Capps <charles.capps@jivesoftware.com>
2015-09-19 15:56:16 -0700

    Auto-scrolling credits.

--- classes/credits_widget.lua
+++ classes/credits_widget.lua
@@ -3,12 +3,15 @@ local display = require("display")
 local widget = require("widget")
 local fonts = require("globals.fonts")
 local math = require("math")
+local timer = require("timer")
 
 local M = {}
 local meta = { __index = M }
 
 function M.new()
-    local creditsWidget = {}
+    local creditsWidget = {
+        currentRow = 1
+    }
     return setmetatable(creditsWidget, meta)
 end
 
@@ -43,15 +46,12 @@ function M:drawTableView()
         hideScrollBar = true,
     }
 
-    local NUM_ROWS = 1
-
-    for i = 1, NUM_ROWS do
+    for i = 1, #M.renderRows do
         tv:insertRow {
             rowHeight = display.contentHeight,
             rowColor = {default = {0, 0, 0, 0}, over = {0, 0, 0, 0}}
         }
     end
-
 
     return tv
 end
@@ -73,6 +73,30 @@ function M:getOnRowRender()
     end
 end
 
+function M:animateCredits()
+    if not self.tableView then
+        return
+    end
+
+    if type(self.currentRow) ~= "number" then
+        return
+    end
+
+    if self.currentRow >= #M.renderRows then
+        return
+    end
+
+    local function onComplete()
+        self.currentRow = self.currentRow + 1
+
+        self:animateCredits()
+    end
+
+    timer.performWithDelay(3000, function()
+        self.tableView:scrollToIndex(self.currentRow + 1, 2000, onComplete)
+    end)
+end
+
 function M.drawCreditsPage(opts)
     local group = display.newGroup()
     group.x = display.contentCenterX
@@ -84,24 +108,38 @@ function M.drawCreditsPage(opts)
         fontSize = opts.titleSize or 64
     }
     titleView:setFillColor(1, 1, 1)
-    titleView.y = -300
+    titleView.y = -350
 
     local textView = display.newText {
         text = opts.text,
         font = fonts.DEFAULT_FONT,
-        fontSize = opts.textSize or 40
+        fontSize = opts.textSize or 52
     }
     textView:setFillColor(1, 1, 1)
-    textView.y = -200
+    textView.y = -250
 
     group:insert(titleView)
     group:insert(textView)
 
+    local subtitleView
+    if opts.subtitle then
+        subtitleView = display.newText {
+            text = opts.subtitle,
+            font = fonts.DEFAULT_FONT,
+            fontSize = opts.subtitleSize or 48
+        }
+        subtitleView.anchorY = 0
+        subtitleView:setFillColor(1, 1, 1)
+        subtitleView.y = textView.y + textView.contentHeight / 2 + 10
+        group:insert(subtitleView)
+    end
+
     if (opts.image and opts.imageWidth and opts.imageHeight) then
-        local IMAGE_PAD = 25
+        local IMAGE_PAD = 75
         local imageView = display.newImageRect(opts.image, opts.imageWidth, opts.imageHeight)
         imageView.x = 0
-        imageView.y = math.max(0, opts.imageHeight / 2 + textView.y + textView.contentHeight / 2 + IMAGE_PAD) -- Image should not spill into the above text
+        local prevView = subtitleView or textView
+        imageView.y = opts.imageHeight / 2 + prevView.y + prevView.contentHeight / 2 + IMAGE_PAD -- Image should not spill into the above text
         group:insert(imageView)
     end
 
@@ -114,12 +152,25 @@ function M.drawCopyrightPage()
         text = "Ghostwriters © 2015 Charles Capps",
         image = "images/cthulhu_board.jpg",
         imageWidth = 600,
+        imageHeight = 600,
+        textSize = 40
+    }
+end
+
+function M.drawArtistPage()
+    return M.drawCreditsPage {
+        title = "Original Artwork",
+        text = "Joao Fiuza",
+        subtitle = "https://inkognit.artstation.com",
+        image = "images/professor_default.png",
+        imageWidth = 600,
         imageHeight = 600
     }
 end
 
 M.renderRows = {
-    M.drawCopyrightPage
+    M.drawCopyrightPage,
+    M.drawArtistPage
 }
 
 return M
--- scenes/credits_scene.lua
+++ scenes/credits_scene.lua
@@ -27,6 +27,7 @@ function scene:show( event )
         -- Called when the scene is still off screen (but is about to come on screen).
     elseif ( phase == "did" ) then
         scene_helpers.onDidShowScene(self)
+        self.creditsWidget:animateCredits()
     end
 end
 
